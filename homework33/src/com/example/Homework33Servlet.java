06be031140258b6dbc9a3c47971835884193fcad
Nikita <Nikita@Nikita-PC>
2016-06-07 14:21:09 +0300

    Dz3.3

---
+++ homework33/src/com/example/Homework33Servlet.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Homework33Servlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setCharacterEncoding("UTF-8");
+		  int[][] Mas = new int[8][5];
+		  for(int i=0;i<Mas.length;i++){
+	            for(int j=0;j<Mas[i].length;j++){
+	                Mas[i][j]=(int)(Math.random()*90)+10;
+	                resp.getWriter().print(Mas[i][j]+" ");
+	            }
+	            resp.getWriter().println("<html><body><br></body></html>");
+	        }
+
+			
+		};
+}
